8d30978897c005edfaeaa5ec61b4dca4c549c854
OpenEsi2 <katrame_@hotmail.com>
2020-03-08 18:28:22 +0100

    Restore Darkness

--- lib/python/Screens/EventView.py
+++ lib/python/Screens/EventView.py
@@ -33,7 +33,7 @@ class EventViewContextMenu(Screen):
 			})
 
 		try:
-			if config.skin.primary_skin.value.startswith('SATtek-EleganceBLUE/'):
+			if config.skin.primary_skin.value.startswith('Darkness/'):
 				count = 0
 				for entry in menu:
 					menu[count] = ("        " + entry[0], entry[1])
